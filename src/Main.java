84328c0d2b973e05345c9197e73e1d871b561f27
Seoklim <ksxl1107@naver.com>
2022-08-03 11:19:29 +0900

    123

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,11 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("성적관리프로그램 시작");
-		Student jimin = new Student(); // 주소를 호출해야함 (멤버변수)
-		Input input = new Input(); input.input(jimin);
-		Calc calc = new Calc();   calc.calc(jimin);
-		Output output = new Output(); output.output(jimin);
+		Student [] array = new Student[2]; // 주소를 호출해야함 (멤버변수)
+		
+		Input input = new Input(); input.input(array);
+		Calc calc = new Calc();   calc.calc(array);
+		Output output = new Output(); output.output(array);
 		
 		
 		System.out.println("성적관리프로그램 끝");
--- src/Output.java
+++ src/Output.java
@@ -1,12 +1,13 @@
 
 public class Output {
-	void output(Student student) {
+	void output(Student[] array) {
 		printLabel(); // Call by name
-		System.out.printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\t%.2f\t%c\n",
-										student.ranking, student.hakbun, student.name,
-											student.kor, student.eng, student.mat, student.tot,
-												student.avg, student.grade);
+		for (int i = 0; i < array.length; i++) {
+			System.out.printf("%d\t%s\t%s\t%d\t%d\t%d\t%d\t%.2f\t%c\n", array[i].ranking, array[i].hakbun,
+					array[i].name, array[i].kor, array[i].eng, array[i].mat, array[i].tot, array[i].avg, array[i].grade);
+		}
 	}
+
 	void printLabel() { // 메소드안의 메소드
 		System.out.println("<<<<센텀정보고등학교 성적관리프로그램>>>>");
 		System.out.println("등수\t학번\t이름\t국어\t영어\t수학\t총점\t평균\t평점");
