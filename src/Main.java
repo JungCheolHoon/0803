555220f90293c9e78f469b5864729ac2f628ae76
Seoklim <ksxl1107@naver.com>
2022-08-03 10:33:34 +0900

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("성적관리프로그램 시작");
+		Student jimin = new Student();
+		Input input = new Input(); input.input(jimin);
+		Calc calc = new Calc();   calc.calc(jimin);
+		Output output = new Output(); output.output(jimin);
+		
+		
+		System.out.println("성적관리프로그램 끝");
+	}
+}
