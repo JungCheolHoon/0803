2a24857a2c1d1c16501628ceb1deeb05e1999aaa
JssKaph <tntmd2658@gmail.com>
2022-08-03 10:35:46 +0900

    fix

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("성적관리프로그램 시작");
-		Student jimin = new Student();
+		Student jimin = new Student(); // 주소를 호출해야함 (멤버변수)
 		Input input = new Input(); input.input(jimin);
 		Calc calc = new Calc();   calc.calc(jimin);
 		Output output = new Output(); output.output(jimin);
